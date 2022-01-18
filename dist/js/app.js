0351d6ffca133d7d43930f9f705be0a34e5ed357
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2022-01-18 16:20:19 +0300

    Init commit

---
+++ dist/js/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function healthIndicator(player) {
+  if (player.health > 50) {
+    return 'healthy';
+  }
+
+  if (player.health < 15) {
+    return 'critical';
+  }
+
+  return 'wounded';
+}
